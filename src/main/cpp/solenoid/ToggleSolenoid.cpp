03a86d4d39dd1063c824ec33ce61ca9d823b2245
FRC-4309-DEV <FRC.4309.DEV@gmail.com>
2019-03-19 19:52:17 -0700

    cleaned up code, changed arm power

--- src/main/cpp/solenoid/ToggleSolenoid.cpp
+++ src/main/cpp/solenoid/ToggleSolenoid.cpp
@@ -3,8 +3,7 @@
 #include <frc/DoubleSolenoid.h>
 
 void Robot::ToggleSolenoid(frc::DoubleSolenoid& solenoid) {
-    //moves solenoid based on bool state
+    //moves solenoid based on current state
     bool state=(solenoid.Get()==frc::DoubleSolenoid::Value::kReverse);
     state ? solenoid.Set(frc::DoubleSolenoid::Value::kForward) : solenoid.Set(frc::DoubleSolenoid::Value::kReverse);
-    //state=!state; //invert state of solenoid
 }
